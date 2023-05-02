de05253bb44bc03f191707f3991e80f30a34ba87
Girish Bhangale <gbhangale@halfpenny.com>
2023-05-02 17:47:23 +0530

    Add project files.

---
+++ LogsQueryClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Monitor.Query;
+using Azure.Monitor.Query.Models;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string tenantId = "";
+        string clientId = "";
+        string clientSecret = "";
+        string workspaceId = "";
+        string query = "AzureDiagnostics | where ResourceProvider =~ 'MICROSOFT.KEYVAULT'|where OperationName =='SecretGet' | where identity_claim_scp_s == 'user_impersonation' | where httpStatusCode_d == 200 ";
+
+        var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+        var client = new LogsQueryClient(credential);
+
+        Azure.Response<LogsQueryResult> response = await client.QueryWorkspaceAsync(
+        workspaceId,
+        query,
+        new QueryTimeRange(TimeSpan.FromDays(15)));
+
+        LogsTable table = response.Value.Table;
+
+        foreach (var row in table.Rows)
+        {
+            Console.WriteLine(row["OperationName"] + " | " + row["ResourceGroup"] + " | " + row["CallerIPAddress"] + " | " + (row["requestUri_s"].ToString().Split("/")[4]));
+        }
+    }
+}
